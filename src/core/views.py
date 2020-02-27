d57b10156d83a6ffc162233237f74ec95884acc2
Ferrandeau Mathieu <m.ferrandeau@hotmail.fr>
2020-02-27 16:10:20 +0100

    Views manipulation

--- src/core/views.py
+++ src/core/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 # third party imports
+from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import generics
 
 from .serializers import PostSerializer
 from .models import Post
+
 
 
 class TestView(APIView):
@@ -27,3 +30,34 @@ class TestView(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors)
+
+
+class PostView(mixins.ListModelMixin,
+               mixins.CreateModelMixin,
+               generics.GenericAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    # def perform_create(self, serializer):
+    #     # send an email
+    #     serializer.save()
+
+
+class PostCreateView(mixins.ListModelMixin,
+                     generics.CreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class PostListCreateView(generics.ListCreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
