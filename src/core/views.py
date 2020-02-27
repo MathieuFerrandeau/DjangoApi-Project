43570d45326b6cc10c62ecaad8ade56050d0ad1a
Ferrandeau Mathieu <m.ferrandeau@hotmail.fr>
2020-02-27 11:50:29 +0100

    Serializers manipulation

--- src/core/views.py
+++ src/core/views.py
@@ -5,18 +5,19 @@ from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .serializers import PostSerializer
+from .models import Post
+
 
 class TestView(APIView):
     def get(self, request, *args, **kwargs):
-        data = {
-            'name': 'jhon',
-            'age': '33'
-        }
-        return Response(data)
+        qs = Post.objects.all()
+        serializer = PostSerializer(qs, many=True)
+        return Response(serializer.data)
 
-"""def test_view(request):
-    data = {
-        'name': 'jhon',
-        'age': '33'
-    }
-    return JsonResponse(data)"""
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
